be4428489caf33586dab5e764ac7576f73c52cf4
Carlos-Carsdfj <carsdfj@gmail.com>
2022-09-15 11:40:10 -0400

    Change index with urlencoded

--- index.js
+++ index.js
@@ -8,44 +8,44 @@ const app = express()
 db.connected()
 app.use(cors())
 app.use(express.json())
-
+app.use(express.urlencoded())
 app.use('/', express.static(__dirname + '/public'))
 
-app.get('/',(request,response)=>{
+app.get('/', (request, response) => {
   response.render('index.html')
 })
 
-app.get('/:route', async(req,res)=>{
+app.get('/:route', async (req, res) => {
   const { route } = req.params
-  try{
-    let url = await Weburl.findOne({shortLink:route})
+  try {
+    let url = await Weburl.findOne({ shortLink: route })
     res.redirect(url.urlOriginal)
-  }catch(error){
-    res.status(404).json({error:'url not saved'})
-  }  
+  } catch (error) {
+    res.status(404).json({ error: 'url not saved' })
+  }
 })
 
-app.post('/create', async(req, res)=>{
+app.post('/create', async (req, res) => {
   const { urlOriginal } = req.body
   const { hostname } = req
 
-  if(urlOriginal ===''){
-    return res.status(400).json({error: 'url is missing '})
+  if (urlOriginal === '') {
+    return res.status(400).json({ error: 'url is missing ' })
   }
-  try{
+  try {
     let url = await Weburl.findOne({ urlOriginal })
-    if(url){
+    if (url) {
       return res.json({ shortLink: `${hostname}/${url.shortLink}` })
     }
-    url = new Weburl({ urlOriginal, shortLink: uniqueUrl() })  
+    url = new Weburl({ urlOriginal, shortLink: uniqueUrl() })
     url.save()
-    return res.json({shortLink: `${hostname}/${url.shortLink}` })
-  }catch(error){
-    res.status(500).json({ error:error.message })
+    return res.json({ shortLink: `${hostname}/${url.shortLink}` })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
   }
 })
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`Servidor web escuchando en el puerto ${PORT}`)
 })
