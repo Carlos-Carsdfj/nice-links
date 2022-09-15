e5cb63033d539bac06a1e3318a79260031ff566e
Carlos-Carsdfj <carsdfj@gmail.com>
2022-09-15 12:08:18 -0400

    Fix fetch at home page

--- public/script.js
+++ public/script.js
@@ -2,34 +2,29 @@ const $form = document.querySelector('#shortlink-form')
 const $data = document.querySelector('#input-link')
 const $ancho = document.querySelector('#shortlink-ancho-form')
 
-
-
-$form.addEventListener('submit',(ev)=>{
-  
+$form.addEventListener('submit', (ev) => {
   ev.preventDefault()
   const data = {
-    urlOriginal:$data.value
+    urlOriginal: $data.value,
   }
-  fetch('/create',{
+  fetch('/api/create', {
     method: 'POST',
     mode: 'cors',
     cache: 'no-cache',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    redirect: 'follow', 
-    referrerPolicy: 'no-referrer', 
-    body:JSON.stringify(data) 
-  } )
-    .then(response => response.json())
-    .then(json => {
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((json) => {
       console.log(json)
-      const { shortLink }  = json
-      if(shortLink){
+      const { shortLink } = json
+      if (shortLink) {
         $ancho.text = shortLink
-        $ancho.href = 'http://'+shortLink
+        $ancho.href = 'http://' + shortLink
       }
     })
 })
-
-
